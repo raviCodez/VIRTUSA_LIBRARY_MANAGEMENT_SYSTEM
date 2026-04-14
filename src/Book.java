2f2c5e1700050dcbc9466c9652468e016915735b
ravi <ravivarmanb025@gmail.com>
2026-04-14 21:08:42 +0530

    added Book and member class

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    
+    String title;
+    String author;
+    String genre;
+    boolean isAvailable;
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.isAvailable = true; 
+    }
+
+}
---
+++ src/Member.java
@@ -0,0 +1,25 @@
+
+import java.util.List;
+
+public class Member {
+    private String username;
+    private List<Book> borrowedBooks;
+
+    Member(String username) {
+        this.username = username;
+        this.borrowedBooks = new java.util.ArrayList<>();
+    }
+    public boolean borrowedBooks(Book book){
+        if(borrowedBooks.size()>5 && book.isAvailable){
+            borrowedBooks.add(book);
+            book.isAvailable = false;
+            return true;
+        }
+        return false;
+    }
+
+    public void returnBook(Book book){
+        borrowedBooks.remove(book);
+        book.isAvailable = true;
+    }
+}
