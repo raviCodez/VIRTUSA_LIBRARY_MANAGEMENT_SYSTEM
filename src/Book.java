62ef36646d1fa8efc9ca2a886d6acb5c3e332a0b
RAVIVARMAN <ravivarmanb025@gmail.com>
2026-04-23 15:26:31 +0530

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,15 +1,21 @@
+import java.time.LocalData;
+
 public class Book {
     
     String title;
     String author;
     String genre;
     boolean isAvailable;
+    LocalDate dueDate;
+    String borrowedby;
 
     public Book(String title, String author, String genre) {
         this.title = title;
         this.author = author;
         this.genre = genre;
         this.isAvailable = true; 
+        this.dueDate=null;
+        this.borrowedBy=null;
     }
 
 }
