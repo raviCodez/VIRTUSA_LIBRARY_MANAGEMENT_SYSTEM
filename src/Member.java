e131dec8b8f2dbb0fbff0475a14798204c9494b4
RAVIVARMAN <ravivarmanb025@gmail.com>
2026-04-23 15:27:09 +0530

    Update Member.java

--- src/Member.java
+++ src/Member.java
@@ -1,4 +1,6 @@
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 public class Member {
@@ -10,17 +12,36 @@ public class Member {
         this.username = username;
         this.borrowedBooks = new java.util.ArrayList<>();
     }
-    public boolean borrowedBooks(Book book){
-        if(borrowedBooks.size()>5 && book.isAvailable){
+    public boolean borrowBooks(Book book){
+        if(borrowedBooks.size()<5 && book.isAvailable){
             borrowedBooks.add(book);
             book.isAvailable = false;
+
+            book.borrowedBy = this.username;
+
+            book.dueDate = java.time.LocalDate.now().plusWeeks(2);
+
+            System.out.println("Book borrowed successfully. Due date: " + book.dueDate);
             return true;
         }
         return false;
     }
 
     public void returnBook(Book book){
-        borrowedBooks.remove(book);
-        book.isAvailable = true;
+         LocalDate today = LocalDate.now();
+
+    if (book.dueDate != null && today.isAfter(book.dueDate)) {
+
+        long daysLate = ChronoUnit.DAYS.between(book.dueDate, today);
+        long fine = daysLate * 2;
+
+        System.out.println("Late by " + daysLate + " days.");
+        System.out.println("Fine: ₹" + fine);
     }
+
+    borrowedBooks.remove(book);
+    book.isAvailable = true;
+    book.dueDate = null;
+    book.borrowedBy = null;
+}
 }
